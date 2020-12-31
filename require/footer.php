4c5a752222a92725fa3162d6b218bd4bb15ad8e0
jayant-ism <52823326+jayant-ism@users.noreply.github.com>
2020-12-31 12:13:27 +0530

    'as'

--- require/footer.php
+++ require/footer.php
@@ -13,6 +13,14 @@
                     <div class="footer_head">
                         <p>Contact Us</p>
                     </div>
+                    
+                    <div class="head_info">
+                        <p>Phone: 03262235870</p>
+                        <p>Email: ciie@iitism.ac.in </p>
+                        <p> Address: CIIE  IIT (ISM) Dhanbad<br> Jharkhand 826004 </p>
+                    </div>
+                    
+                    
                 </div>
             </div>
             <div class="copyright">
